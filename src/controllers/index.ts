1d421b63dd5068a5d5532ded0485310d06c104d4
Fukushiro <jpflc2301@gmail.com>
2022-05-14 18:57:17 -0300

    busca de subreddit por like criado

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,6 +1,6 @@
 export function checkNull(arr: Array<any>) {
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] == undefined) {
+    if (arr[i] == undefined || arr[i] == null || arr[i] == '') {
       return false;
     }
   }
