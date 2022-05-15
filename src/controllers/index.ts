646bfea0695f5d12fb21404d9025fe12749edfd6
Fukushiro <jpflc2301@gmail.com>
2022-05-15 01:06:57 -0300

    correcao

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,6 +1,6 @@
 export function checkNull(arr: Array<any>) {
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] == undefined || arr[i] == null || arr[i] == '') {
+    if (arr[i] == undefined || arr[i] == null) {
       return false;
     }
   }
