4f7739a067de5d10eac2991230f389b24c204b27
DESKTOP-ELGSLMU\Fukushiro <jpflc2301@gmail.com>
2022-06-06 21:41:58 -0300

    mudancas no docker

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,13 @@
 FROM node:17-alpine
 WORKDIR /app
 COPY package.json ./
+COPY yarn.lock ./
 RUN yarn install
 COPY tsconfig.json ./
 
 COPY src ./src
 COPY .env ./
+COPY dev.sqlite3 ./
 EXPOSE 8000
 
 
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
-import express from 'express';
-import cors from 'cors';
-import routes from './routes';
-const path = require('path');
-console.log(path.resolve('config', 'config.ts'));
+import express from "express";
+import cors from "cors";
+import routes from "./routes";
+const path = require("path");
+console.log(path.resolve("config", "config.ts"));
 
 //create app
 const app = express();
 //port to use
 const PORT = 8000;
 //configure express
-app.use(cors({ origin: '*' }));
+app.use(cors({ origin: "*" }));
 app.use(express.json());
-app.get('/', (req, res) => res.send('Express + TypeScript Server'));
+app.get("/", (req, res) => res.send("Express + TypeScript Server"));
 routes(app);
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
